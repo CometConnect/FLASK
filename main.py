4ce2067023e33323c84770d7f6b9eb83c7509b24
FloatingComet62 <shubh2007rai@gmail.com>
2022-12-29 21:17:14 +0530

    2

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    return render_template("login.html")
+def home():
+    name = "Alex"
+    age = "12"
+    return render_template('index.html' , name = name , age = age)
 
-@app.route("/main")
-def hello():
-    name = request.args.get("name")
-    return render_template("index.html", name=name)
+# define the route to father webpage
+@app.route("/father")
+def father():
+    name = "Mark"
+    age = 40
+    return render_template('father.html' , name = name , age = age)
 
-if __name__ == "__main__":
+# define the route to mother webpage
+@app.route("/mother")
+def mother():
+    name = "Erica"
+    age = 37
+    return render_template('mother.html' , name = name , age = age)
+
+# define the route to friends webpage
+@app.route("/friend")
+def friend():
+    name = "Victor"
+    age = 12
+    return render_template('friend.html' , name = name , age = age)
+
+if __name__  ==  '__main__':
     app.run(debug=True)
